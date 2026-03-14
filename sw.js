d21d6444952977413eb73c4177c008bbf0fa46d3
Hoinz <czarararek1@gmail.com>
2026-03-14 07:33:40 +0100

    inactive services fix #2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pawsome-v3';
+const CACHE='pawsome-v4';
 const ASSETS=['/','index.html','usluga.html','umow-wizyte.html','shared.css','booking.css','booking.js','site-content.js','firebase-config.js','fonts/dmsans-latin.woff2','fonts/dmsans-latin-ext.woff2','fonts/playfair-latin.woff2','fonts/playfair-latin-ext.woff2','fonts/playfair-italic-latin.woff2','fonts/playfair-italic-latin-ext.woff2'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
